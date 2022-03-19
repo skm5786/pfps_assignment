2073c54027a4303671586898f172b3dcc8884a39
skm5786 <sumitthakral3@gmail.com>
2022-03-19 08:11:02 +0530

    Initial Commit

---
+++ pfps assignment/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pfps assignment
+//
+//  Created by sumit thakral on 19/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
