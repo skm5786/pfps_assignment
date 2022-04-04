9517399c3b992b156b9332b073d9a49274e328db
skm5786 <sumitthakral3@gmail.com>
2022-04-04 09:09:59 +0530

    commit

--- question 1/main.c
+++ question 1/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int a=2,b=2,r;
+    int a=31,b=2,r;
     printf("press 1 for hardcoded inputs\npress 2 for inputs supplied by the user\n");
     scanf("%d",&r);
     if (r==1) {
